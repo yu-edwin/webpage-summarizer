251502cbfd657543ca07f1e2cb39335be411aa19
yu-edwin <edwinjcy@gmail.com>
2025-04-13 08:39:13 -0700

    implementing mock

--- AI-Summarizer/src/tests/mockChrome.jsx
+++ AI-Summarizer/src/tests/mockChrome.jsx
@@ -5,26 +5,24 @@ const storage = {}
 /**
  * mocks the chrome.storage functionality
  * requires chrome.storage.sync.get() and chrome.storage.sync.set()
- *
+ * chrome.storage.sync.reset for reseting storage before each test case
  */
 export const mockChromeStorageSync = {
-    "sync": {
-        "get": vi.fn((keys) => {
-            const out = {}
-            for (const key of keys) {
-                out[key] = storage[key];
-            }
-            return Promise.resolve(out);
-        }),
-        "set": vi.fn((dict) => {
-            for (const key in dict) {
-                storage[key] = dict[key];
-            }
-        }),
-        "reset": vi.fn(() => {
-           for (const key in storage) {
-                delete storage[key];
-           } 
-        })
-    }
+    "get": vi.fn((keys, callback) => {
+        const out = {}
+        for (const key of keys) {
+            out[key] = storage[key];
+        }
+        callback(out);
+    }),
+    "set": vi.fn((dict) => {
+        for (const key in dict) {
+            storage[key] = dict[key];
+        }
+    }),
+    "reset": vi.fn(() => {
+        for (const key in storage) {
+            delete storage[key];
+        } 
+    })
 }
