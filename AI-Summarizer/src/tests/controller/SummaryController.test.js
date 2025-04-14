2b525bfca0e851d6e4e90709a98f0834aa69b203
yu-edwin <edwinjcy@gmail.com>
2025-04-14 06:07:33 -0700

    implementin SummaryController video test cases

---
+++ AI-Summarizer/src/tests/controller/SummaryController.test.js
@@ -0,0 +1,48 @@
+import { describe, it, expect, beforeAll, beforeEach } from "vitest";
+import { SummaryController } from "../../controller/SummaryController.jsx";
+import { mockChromeStorageSync,
+        validVideo,
+        invalidVideo } from "../mockChrome.jsx";
+import { render, renderHook, screen } from "@testing-library/react";
+
+describe("SummaryController", () => {
+    //. loading mocked chrome
+    beforeAll(() =>{
+        global.chrome = {}
+        global.chrome.storage = {}
+        global.chrome.storage.sync = mockChromeStorageSync;
+        global.chrome.Tabs = {}
+    });
+    
+    // clear storage before each test case
+    beforeEach(() => {
+        chrome.storage.sync.reset();
+    });
+    
+    it("tests getting video url", () => {
+        var summaryController = renderHook(SummaryController);
+        global.chrome.Tabs.query = (_, callback) => callback([validVideo]);
+
+        expect(summaryController.result.getUrl()).toEqual(validVideo);
+    });
+
+    it("tests checking if link is a valid video with a real link", () => {
+        var summaryController = renderHook(SummaryController);
+        expect(summaryController.result.isVideo(validVideo)).toEqual(true);
+    });
+
+    it("tests checking if link is a valid video with a fake link", () => {
+        var summaryController = renderHook(SummaryController);
+        expect(summaryController.result.isVideo(invalidVideo)).toEqual(false);
+    });
+
+    it("tests the whole SummaryController, with a valid video", () => {
+        global.chrome.Tabs.query = (_, callback) => callback([validVideo]);
+        var summaryController = renderHook(SummaryController);
+
+        expect(summaryController.result.current.status).toEqual(200);
+        expect(summaryController.result.current.url).toEqual(validVideo);
+        expect(summaryController.result.current.response).toBeTypeOf("string");
+    });
+});
+
