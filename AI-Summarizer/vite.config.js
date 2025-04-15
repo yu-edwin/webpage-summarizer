d1fdd44f3ab43ab85e10e0c31dad35ddf5749634
yu-edwin <edwinjcy@gmail.com>
2025-04-15 12:16:39 -0700

    updating test timeout

--- AI-Summarizer/vite.config.js
+++ AI-Summarizer/vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
     plugins: [react()],
     test: {
         environment: "jsdom",
+        testTimeout: 60000,
     },
     esbuild: {
         minifyIdentifiers: false,
