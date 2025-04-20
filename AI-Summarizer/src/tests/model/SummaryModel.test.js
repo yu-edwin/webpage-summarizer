f7e06bc068d79d3808ddb9d382e89f4e3937d1ba
yu-edwin <edwinjcy@gmail.com>
2025-04-20 00:26:09 -0700

    updating SummaryModel test cases for webpage summary with google

--- AI-Summarizer/src/tests/model/SummaryModel.test.js
+++ AI-Summarizer/src/tests/model/SummaryModel.test.js
@@ -91,32 +91,32 @@ describe("SummaryModel", () => {
 
 
 
-    // it("tests getting webpage summary with valid webpage and valid key using Google", async () => {
-    //     const summaryModel = new SummaryModel();
-    //     const summary = await summaryModel.getSummaryGoogle(links.validWebsite, keys.valid);
-    //     setTimeout(() => {
-    //         expect(GoogleGenAI).toHaveBeenCalledWith({ apiKey: keys.valid });
-    //         expect(summary).toEqual("correct summary");
-    //     }, 50);
-    // });
+    it("tests getting webpage summary with valid webpage and valid key using Google", async () => {
+        const summaryModel = new SummaryModel();
+        const summary = await summaryModel.getSummaryGoogle("correct input", keys.valid, "");
+        expect(GoogleGenAI).toHaveBeenCalledWith({ apiKey: keys.valid });
+        expect(summary).toEqual("correct summary");
+    });
 
-    // it("tests getting webpage summary with  invalid key using Google", async () => {
-    //     const summaryModel = new SummaryModel();
-    //     const summary = await summaryModel.getSummaryGoogle(links.validWebsite, keys.invalid);
-    //     setTimeout(() => {
-    //         expect(GoogleGenAI).toHaveBeenCalledWith({ apiKey: keys.invalid });
-    //         expect(summary).toEqual("Failed to configure!");
-    //     }, 50);
-    // });
+    it("tests getting webpage summary with invalid key using Google", async () => {
+        const summaryModel = new SummaryModel();
+        const summary = await summaryModel.getSummaryGoogle("correct input", keys.invalid, "");
+        expect(GoogleGenAI).toHaveBeenCalledWith({ apiKey: keys.invalid });
+        expect(summary).toEqual("Failed to generate video summary!");
+    });
 
-    // it("tests getting webpage summary with invalid webpage using Google", async () => {
-    //     const summaryModel = new SummaryModel();
-    //     const summary = await summaryModel.getSummaryGoogle(links.invalidWebsite, keys.valid);
-    //     setTimeout(() => {
-    //         expect(GoogleGenAI).toHaveBeenCalledWith({ apiKey: keys.invalid });
-    //         expect(summary).toEqual("Failed to generate video summary!");
-    //     }, 50);
-    // });
+    it("tests getting webpage summary with missing key using Google", async () => {
+        const summaryModel = new SummaryModel();
+        const summary = await summaryModel.getSummaryGoogle("correct input", keys.missing, "");
+        expect(summary).toEqual("Missing API key!");
+    });
+
+    it("tests getting webpage summary with invalid webpage using Google", async () => {
+        const summaryModel = new SummaryModel();
+        const summary = await summaryModel.getSummaryGoogle("wrong input", keys.valid, "");
+        expect(GoogleGenAI).toHaveBeenCalledWith({ apiKey: keys.invalid });
+        expect(summary).toEqual("Failed to generate video summary!");
+    });
 
 
 
@@ -124,28 +124,27 @@ describe("SummaryModel", () => {
     it("tests getting video summary with valid link and valid key", async () => {
         const summaryModel = new SummaryModel();
         const summary = await summaryModel.getSummaryVideo(links.validVideo, keys.valid, "");
-        expect(GoogleGenAI).toHaveBeenCalledWith({ apiKey: keys.valid });
+        expect(GoogleGenAI).toHaveBeenLastCalledWith({ apiKey: keys.valid });
         expect(summary).toEqual("correct summary");
     });
 
     it("tests getting video summary with invalid key", async () => {
         const summaryModel = new SummaryModel();
         const summary = await summaryModel.getSummaryVideo(links.validVideo, keys.invalid, "");
-        expect(GoogleGenAI).toHaveBeenCalledWith({ apiKey: keys.invalid });
+        expect(GoogleGenAI).toHaveBeenLastCalledWith({ apiKey: keys.invalid });
         expect(summary).toEqual("Failed to generate video summary!");
     });
 
-    it("tests getting video summary with invalid key", async () => {
+    it("tests getting video summary with missing key", async () => {
         const summaryModel = new SummaryModel();
         const summary = await summaryModel.getSummaryVideo(links.validVideo, keys.missing, "");
-        expect(GoogleGenAI).toHaveBeenCalledWith({ apiKey: keys.invalid });
         expect(summary).toEqual("Missing API key!");
     });
 
     it("tests getting video summary with invalid link", async () => {
         const summaryModel = new SummaryModel();
         const summary = await summaryModel.getSummaryVideo(links.invalidVideo, keys.valid, "");
-        expect(GoogleGenAI).toHaveBeenCalledWith({ apiKey: keys.valid });
+        expect(GoogleGenAI).toHaveBeenLastCalledWith({ apiKey: keys.valid });
         expect(summary).toEqual("Failed to generate video summary!");
     });
 });
